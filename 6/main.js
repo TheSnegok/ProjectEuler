4d11ae2eb31c65d52cb30442ae40a3fc782c537d
Max <lord180499@gmail.com>
2021-07-22 23:21:33 +0300

    6 tasks comleted

--- 6/main.js
+++ 6/main.js
@@ -1,18 +1,12 @@
 "use strict";
 
-let firstSum = 0, secondSum = 0, i = 1, f = 1, answer = 0;
+let firstSum = 0, secondSum = 0, i = 1;
 
 while (i < 101) {
     firstSum = firstSum + (i**2);
+    secondSum = secondSum + i;
     i++;
 }
+secondSum = secondSum ** 2
 
-while (f < 101) {
-    secondSum = secondSum + f;
-    f++;
-}
-secondSum = secondSum ** 2;
-
-answer = secondSum - firstSum;
-
-console.log("Сумма квадратов: " + firstSum, "Квадрат сумы: " + secondSum, "Ответ: " + answer);
+console.log("Сумма квадратов: " + firstSum, "Квадрат сумы: " + secondSum, "Ответ: " + (secondSum - firstSum));
