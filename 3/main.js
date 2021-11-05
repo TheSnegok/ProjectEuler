3b3f2deae95081b32730c0ffdaff724000cbc79b
TheSnegok <34038093+TheSnegok@users.noreply.github.com>
2021-11-05 22:32:29 +0300

    Update main.js

--- 3/main.js
+++ 3/main.js
@@ -6,7 +6,7 @@ let Log = (output) => console.log(output);
 
 function priNumber (num) {
     for(let i = 1; i <= num; i++) {
-        (num % 2 == 0 || num % 3 == 0 || num / 5 == 0 || num / 7 == 0) ? Log('Это простое число! ' + i) : Log('Число не является простым! ' + i);
+        (num % 2 == 0 || num % 3 == 0 || num % 5 == 0 || num % 7 == 0) ? Log('Это простое число! ' + i) : Log('Число не является простым! ' + i);
     }
 }
 
